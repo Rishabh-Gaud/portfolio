954ca3c5d51740a3497c7fa3543b32bdc3da4845
Rishabh Kumar Gaud <rishabh.kumbh@gmail.com>
2023-04-30 16:48:14 +0530

    header section added

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -79,7 +79,7 @@ const Header = () => {
               <div className={`${classes.nav__right}`}>
                 <p className=" d-flex align-items-center gap-2 mb-0">
                   {" "}
-                  <i className="ri-phone-line"></i> +088123456789{" "}
+                  <i className="ri-phone-line"></i> +918287233813{" "}
                 </p>
               </div>
             </div>

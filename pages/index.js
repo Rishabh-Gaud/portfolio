663dad84d21dbaacfdb03e4f2275e6f48bcf3651
Rishabh Kumar Gaud <rishabh.kumbh@gmail.com>
2023-05-26 12:06:30 +0530

    work experience added

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
     <Fragment>
       <Hero />
       <Services />
+      <Work />
       <About />
-      {/* <Work /> */}
       <Portfolio />
       <Contact />
     </Fragment>

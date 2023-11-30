7ffc2ec72675807fe827a2a6040b261e4b3ee528
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2023-11-30 11:28:25 +0530

    Update Hero.jsx

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -30,9 +30,18 @@ const Hero = () => {
                   <Link href="#">Hire me</Link>
                 </button> */}
 
-                <button className="secondary__btn">
+{/*                 <button className="secondary__btn">
                   <Link target="_blank" href="https://drive.google.com/file/d/1vXIBkfQTC4L9hGXE7raU-hnskxzgF1zn/view">Download Resume</Link>
-                </button>
+                </button> */}
+                <h2>Connect with me</h2>
+<p align="left">
+<a href="https://linkedin.com/in/rishabhgaud7800" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="rishabhgaud7800" height="30" width="40" /></a>
+<a href="https://www.facebook.com/profile.php?id=100010337352255" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="rishabh gaud" height="30" width="40" /></a>
+<a href="https://instagram.com/riishabhgaud" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="riishabhgaud" height="30" width="40" /></a>
+<a href="https://www.hackerrank.com/rishabh78150891" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerrank.svg" alt="rishabh78150891" height="30" width="40" /></a>
+<a href="https://www.leetcode.com/rishabh-gaud" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="rishabh-gaud" height="30" width="40" /></a>
+<a href="https://discord.gg/Rishabh#9774" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/discord.svg" alt="Rishabh#9774" height="30" width="40" /></a>
+</p>
               </div>
             </div>
           </Col>
